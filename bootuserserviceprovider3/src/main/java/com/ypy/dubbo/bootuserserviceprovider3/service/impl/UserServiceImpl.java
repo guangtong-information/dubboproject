4645d1d300e94e627483d4fd7d4846d996420cbf
Ryan <631811480@qq.com>
2019-08-24 23:21:59 +0800

    负载均衡机制

--- bootuserserviceprovider3/src/main/java/com/ypy/dubbo/bootuserserviceprovider3/service/impl/UserServiceImpl.java
+++ bootuserserviceprovider3/src/main/java/com/ypy/dubbo/bootuserserviceprovider3/service/impl/UserServiceImpl.java
@@ -17,7 +17,8 @@ public class UserServiceImpl implements UserService {
 
     public List<UserAddress> getUserAddressList(String userId) {
 
-        System.out.println("----------验证重试次数1---------");
+//        System.out.println("----------验证重试次数1---------");
+        System.out.println("----------验证负载均衡20883---------");
 
         UserAddress userAddress1 = new UserAddress(1,"北京市1","1","李老师","123456789","Y");
         UserAddress userAddress2 = new UserAddress(2,"北京市2","1","李老师","123456789","N");
@@ -25,11 +26,11 @@ public class UserServiceImpl implements UserService {
         /**
          * 模拟dubbo超时属性的设置
          */
-        try {
+        /*try {
             Thread.sleep(3000);
         } catch (Exception e){
             e.printStackTrace();
-        }
+        }*/
 
         return Arrays.asList(userAddress1,userAddress2);
     }
