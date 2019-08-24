39006e564e8fd6496e07345cb4e8ad9359ef30d7
Ryan <631811480@qq.com>
2019-08-24 23:18:05 +0800

    负载均衡机制

--- bootuserserviceprovider3/src/main/java/com/ypy/dubbo/bootuserserviceprovider3/BootProvidenceApplication3.java
+++ bootuserserviceprovider3/src/main/java/com/ypy/dubbo/bootuserserviceprovider3/BootProvidenceApplication3.java
@@ -4,8 +4,8 @@ import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableDubbo(scanBasePackages = {"com.ypy.dubbo.bootuserserviceprovider2"}) // 开启基于注解的dubbbo功能，主要是用于包扫描，等同于配置文件application.properties中定义：dubbo.scan.base-packages=com.ypy.dubbo.bootuserserviceprovider
-@DubboComponentScan(basePackages = {"com.ypy.dubbo.bootuserserviceprovider2"})
+//@EnableDubbo(scanBasePackages = {"com.ypy.dubbo.bootuserserviceprovider3"}) // 开启基于注解的dubbbo功能，主要是用于包扫描，等同于配置文件application.properties中定义：dubbo.scan.base-packages=com.ypy.dubbo.bootuserserviceprovider
+@DubboComponentScan(basePackages = {"com.ypy.dubbo.bootuserserviceprovider3"})
 @SpringBootApplication
 public class BootProvidenceApplication3 {
 
